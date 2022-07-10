836e91dca3ddf8dfc629039e69fd4d4e03d6f555
pab-mich <pab.michelini@gmail.com>
2022-07-10 16:51:56 +0200

    projects dinamic section done

--- app.js
+++ app.js
@@ -1,7 +1,35 @@
+//responsive menu
 const menu = document.querySelector("#mobile-menu");
 const menuLinks = document.querySelector(".nav-menu");
 
 menu.addEventListener("click", function () {
   menu.classList.toggle("is-active");
   menuLinks.classList.toggle("active");
+});
+
+//dinamic projects
+
+const projectListContainer = document.getElementById("projects-list-container");
+
+projects.forEach((project) => {
+  let projectContent = document.createElement("li");
+  projectContent.className = "projects-list-item";
+  projectContent.innerHTML = `
+                <img src="${project.img}">
+                <div class="projects-list-item-info">
+                <h3>${project.name} </h3>
+                <h6>${project.projectDescription}</h6>
+                <p><small>${project.technologyDescription}</small></p>
+                <p><span><a class="projects-list-item-info-link" href="${project.linkProject}">visit the site</a></span> - <span><a class="projects-list-item-info-link" href="${project.linkcode}">visit the code</a></span>
+                </div>
+            `;
+  projectListContainer.append(projectContent);
+});
+
+//link in logo to go to the top
+//
+const navbarLogo = document.getElementById("navbar-logo");
+
+navbarLogo.addEventListener("click", () => {
+  window.scrollTo(0, 0);
 });
---
+++ projects.js
@@ -0,0 +1,62 @@
+const projects = [
+  {
+    id: 1,
+    img: "https://images.pexels.com/photos/694740/pexels-photo-694740.jpeg",
+    name: " Beats Books Shop",
+    projectDescription: "E-commerce of books",
+    technologyDescription:
+      "Made with html, css, javascript vanilla, integrated with paypal api as payment method.",
+    linkProject: "https://pab-mich.github.io/Beats-Books-Shop/",
+    linkcode: "https://github.com/pab-mich/Beats-Books-Shop",
+  },
+  {
+    id: 1,
+    img: "https://images.pexels.com/photos/694740/pexels-photo-694740.jpeg",
+    name: " Beats Books Shop",
+    projectDescription: "E-commerce of books",
+    technologyDescription:
+      "Made with html, css, javascript vanilla, integrated with paypal api as payment method.",
+    linkProject: "https://pab-mich.github.io/Beats-Books-Shop/",
+    linkcode: "https://github.com/pab-mich/Beats-Books-Shop",
+  },
+  {
+    id: 1,
+    img: "https://images.pexels.com/photos/694740/pexels-photo-694740.jpeg",
+    name: " Beats Books Shop",
+    projectDescription: "E-commerce of books",
+    technologyDescription:
+      "Made with html, css, javascript vanilla, integrated with paypal api as payment method.",
+    linkProject: "https://pab-mich.github.io/Beats-Books-Shop/",
+    linkcode: "https://github.com/pab-mich/Beats-Books-Shop",
+  },
+  {
+    id: 1,
+    img: "https://images.pexels.com/photos/694740/pexels-photo-694740.jpeg",
+    name: " Beats Books Shop",
+    projectDescription: "E-commerce of books",
+    technologyDescription:
+      "Made with html, css, javascript vanilla, integrated with paypal api as payment method.",
+    linkProject: "https://pab-mich.github.io/Beats-Books-Shop/",
+    linkcode: "https://github.com/pab-mich/Beats-Books-Shop",
+  },
+  {
+    id: 1,
+    img: "https://images.pexels.com/photos/694740/pexels-photo-694740.jpeg",
+    name: " Beats Books Shop",
+    projectDescription: "E-commerce of books",
+    technologyDescription:
+      "Made with html, css, javascript vanilla, integrated with paypal api as payment method.",
+    linkProject: "https://pab-mich.github.io/Beats-Books-Shop/",
+    linkcode: "https://github.com/pab-mich/Beats-Books-Shop",
+  },
+  {
+    id: 1,
+    img: "https://images.pexels.com/photos/694740/pexels-photo-694740.jpeg",
+    name: " Beats Books Shop",
+    projectDescription: "E-commerce of books",
+    technologyDescription:
+      "Made with html, css, javascript vanilla, integrated with paypal api as payment method.",
+    linkProject: "https://pab-mich.github.io/Beats-Books-Shop/",
+    linkcode: "https://github.com/pab-mich/Beats-Books-Shop",
+  },
+];
