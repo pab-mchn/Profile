16dfa048ae0e98ed1e7a9fdb6550d5b7d55fa6e6
pab-mich <pab.michelini@gmail.com>
2022-07-11 20:50:24 +0200

    added new plants project

--- projects.js
+++ projects.js
@@ -10,14 +10,15 @@ const projects = [
     linkcode: "https://github.com/pab-mich/Beats-Books-Shop",
   },
   {
-    id: 1,
-    img: "https://images.pexels.com/photos/694740/pexels-photo-694740.jpeg",
-    name: " Beats Books Shop",
-    projectDescription: "E-commerce of books",
+    id: 2,
+    img:
+      "https://imgs.search.brave.com/I2Uhkk-vRYyZ5LvHhcDcGkMsa8fW2Z-Qb1ynu5Xbsj8/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9kLm5l/d3N3ZWVrLmNvbS9l/bi9mdWxsLzE2NzA5/NjcvcGxhbnRzLWlu/dGVyaW9yLWRlc2ln/bi1pbmRvb3ItZ2Fy/ZGVuLmpwZw",
+    name: "Plants App",
+    projectDescription: "website to find info about plants",
     technologyDescription:
-      "Made with html, css, javascript vanilla, integrated with paypal api as payment method.",
-    linkProject: "https://pab-mich.github.io/Beats-Books-Shop/",
-    linkcode: "https://github.com/pab-mich/Beats-Books-Shop",
+      "Made with React Js and with the api created with My JSON Server deployed in heroku",
+    linkProject: "https://github.com/pab-mich/plants-app",
+    linkcode: "https://github.com/pab-mich/plants-app",
   },
   {
     id: 1,
