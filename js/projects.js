07387e88e2aa373aa06acfcc5447a8855f3ddd28
pab-mich <pab.michelini@gmail.com>
2022-07-30 13:07:43 +0200

    new project added

--- js/projects.js
+++ js/projects.js
@@ -4,60 +4,29 @@ const projects = [
     img: "https://images.pexels.com/photos/694740/pexels-photo-694740.jpeg",
     name: " Beats Books Shop",
     projectDescription: "E-commerce of books",
-    technologyDescription:
-      "Made with html, css, javascript vanilla, integrated with paypal api as payment method.",
+    technologyDescription: "Made with html, css, javascript vanilla, integrated with paypal api as payment method.",
     linkProject: "https://pab-mich.github.io/Beats-Books-Shop/",
     linkcode: "https://github.com/pab-mich/Beats-Books-Shop",
   },
   {
     id: 2,
     img:
+      "https://images.pexels.com/photos/3184540/pexels-photo-3184540.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    name: "Venice Films",
+    projectDescription: "Venice-films oficial website",
+    technologyDescription:
+      "Made with React Js and with the api created with My JSON Server deployed in heroku using cloudinary for images and vimeo for videos",
+    linkProject: "https://pab-mich.github.io/venice-films/",
+    linkcode: "https://github.com/pab-mich/venice-films",
+  },
+  {
+    id: 3,
+    img:
       "https://imgs.search.brave.com/I2Uhkk-vRYyZ5LvHhcDcGkMsa8fW2Z-Qb1ynu5Xbsj8/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9kLm5l/d3N3ZWVrLmNvbS9l/bi9mdWxsLzE2NzA5/NjcvcGxhbnRzLWlu/dGVyaW9yLWRlc2ln/bi1pbmRvb3ItZ2Fy/ZGVuLmpwZw",
     name: "Plants App",
     projectDescription: "website to find info about plants",
-    technologyDescription:
-      "Made with React Js and with the api created with My JSON Server deployed in heroku",
+    technologyDescription: "Made with React Js and with the api created with My JSON Server deployed in heroku",
     linkProject: "https://pab-mich.github.io/plants-app/",
     linkcode: "https://github.com/pab-mich/plants-app",
-  },
-  {
-    id: 1,
-    img: "https://images.pexels.com/photos/694740/pexels-photo-694740.jpeg",
-    name: " Beats Books Shop",
-    projectDescription: "E-commerce of books",
-    technologyDescription:
-      "Made with html, css, javascript vanilla, integrated with paypal api as payment method.",
-    linkProject: "https://pab-mich.github.io/Beats-Books-Shop/",
-    linkcode: "https://github.com/pab-mich/Beats-Books-Shop",
-  },
-  {
-    id: 1,
-    img: "https://images.pexels.com/photos/694740/pexels-photo-694740.jpeg",
-    name: " Beats Books Shop",
-    projectDescription: "E-commerce of books",
-    technologyDescription:
-      "Made with html, css, javascript vanilla, integrated with paypal api as payment method.",
-    linkProject: "https://pab-mich.github.io/Beats-Books-Shop/",
-    linkcode: "https://github.com/pab-mich/Beats-Books-Shop",
-  },
-  {
-    id: 1,
-    img: "https://images.pexels.com/photos/694740/pexels-photo-694740.jpeg",
-    name: " Beats Books Shop",
-    projectDescription: "E-commerce of books",
-    technologyDescription:
-      "Made with html, css, javascript vanilla, integrated with paypal api as payment method.",
-    linkProject: "https://pab-mich.github.io/Beats-Books-Shop/",
-    linkcode: "https://github.com/pab-mich/Beats-Books-Shop",
-  },
-  {
-    id: 1,
-    img: "https://images.pexels.com/photos/694740/pexels-photo-694740.jpeg",
-    name: " Beats Books Shop",
-    projectDescription: "E-commerce of books",
-    technologyDescription:
-      "Made with html, css, javascript vanilla, integrated with paypal api as payment method.",
-    linkProject: "https://pab-mich.github.io/Beats-Books-Shop/",
-    linkcode: "https://github.com/pab-mich/Beats-Books-Shop",
   },
 ];
