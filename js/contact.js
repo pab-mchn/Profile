f0b0f2fa2ffd56ad0d273db149a8ef23d3724982
pab-mich <pab.michelini@gmail.com>
2023-04-18 09:19:27 +0100

    final functionality in contact form

--- js/contact.js
+++ js/contact.js
@@ -1,19 +1,25 @@
 const btn = document.getElementById("button");
+const nameValue = document.getElementById("user_name");
+const emailValue = document.getElementById("user_email");
+const messageValue = document.getElementById("message");
 
 document.getElementById("form").addEventListener("submit", function (event) {
   event.preventDefault();
 
   btn.value = "Sending...";
 
-  const serviceID = "service_74fqxoc";
-  const templateID = "template_pomc62r";
+  const serviceID = "service_yz24emb";
+  const templateID = "template_erqv36g";
 
   emailjs.sendForm(serviceID, templateID, this).then(
     () => {
       btn.value = "Send Email";
+      nameValue.value = "";
+      emailValue.value = "";
+      messageValue.value = "";
       Swal.fire({
         icon: "success",
-        title: "Thanks the email was sent",
+        title: "The email was sent",
         showConfirmButton: false,
         timer: 1500,
       });
