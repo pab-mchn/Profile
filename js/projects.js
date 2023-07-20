2f55398d78a60a5ece9d78720f5a4d707bc4a28c
pab-mich <pab.michelini@gmail.com>
2023-07-20 16:59:36 +0100

    ui changes

--- js/projects.js
+++ js/projects.js
@@ -34,4 +34,26 @@ const projects = [
     linkInfo: "https://www.linkedin.com/company/mind-support-e-v/",
     linkProject: "https://mindsupport.eu/en/home",
   },
+  {
+    id: 3,
+    img: "https://res.cloudinary.com/pabcode/image/upload/v1674052784/onTheCode/onthecode_banner_cegflx.jpg",
+    name: "on the code",
+    projectDescription: "Youtube channel about programming tutorials",
+    technologyDescription:
+      "I Made coding javascript tutorials in my youtube chanel, showing step by step how to create coding projects",
+    role: "Role: Content Creator and Mentor. Creation of programming tutorials and project consulting",
+    linkInfo: "https://www.youtube.com/@onthecode/about",
+    linkProject: "https://www.youtube.com/channel/UCrP5xPLzqW3k6ssG8C7lHVw",
+  },
+  {
+    id: 3,
+    img: "https://res.cloudinary.com/pabcode/image/upload/v1674128145/onTheCode/min_support_bannerpng_fbbap6.png",
+    name: "Mind Support",
+    projectDescription: "The free online platform offers peer support and self support options",
+    technologyDescription:
+      "In the Mind Support developer team I work on the creation, development and maintenance of the mobile application and on website maintenance and improvements",
+    role: "Role in te team: Front-End Web Developer",
+    linkInfo: "https://www.linkedin.com/company/mind-support-e-v/",
+    linkProject: "https://mindsupport.eu/en/home",
+  },
 ];
--- js/scoll.js
+++ js/scoll.js
@@ -11,9 +11,9 @@ scrollLinks.forEach((link) => {
 
     if (targetId === "#contact") {
       if (window.innerWidth < 768) {
-        offset = navbarHeight - 420; // decrease the offset by 320 pixels on small screens
+        offset = navbarHeight - 350; // decrease the offset by 320 pixels on small screens
       } else {
-        offset = navbarHeight - (45 * window.innerHeight) / 100; // decrease the offset by 5% of the viewport height on larger screens
+        offset = navbarHeight - (20 * window.innerHeight) / 100; // decrease the offset by 5% of the viewport height on larger screens
       }
     } else {
       offset = navbarHeight;
